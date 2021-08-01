616b879e1ad537ecc4ac3fd48d0d76cd02ce337c
septem <qitengyuxi@gmail.com>
2021-08-01 20:44:25 +0900

    デザイン修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -34,7 +29,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -59,8 +53,7 @@ class PostsController < ApplicationController
     def set_post
       @post = Post.find(params[:id])
     end
-
     def post_params
       params.require(:post).permit(:content)
-    end
+  end
 end
